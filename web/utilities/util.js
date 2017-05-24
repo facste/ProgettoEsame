eb15e6300c3b7f6f945a69a649f29d0c4f1209b7
facste <20009959@studenti.uniupo.it>
2017-05-24 13:49:22 +0200

    Modifica util.js con aggiunta controlli sui dati

--- web/utilities/util.js
+++ web/utilities/util.js
@@ -5,8 +5,16 @@ $(document).ready(function() {
     $('form').submit(function () {
         var user = $.trim($('#user').val());
         var psw = $.trim($('#psw').val());
-        if (user === '' || psw === '') {
-            alert('Username or password are empty.');
+        if (user === '' || user.contains(";")) {
+            alert('Username vuoto o contenente caratteri illegatli [; ,]');
+            return false;
+        }
+        if (psw === '' || psw.contains(";")) {
+            alert('Password vuoto o contenente caratteri illegatli [; ,]');
+            return false;
+        }
+        if (psw.length<=7) {
+            alert('Password troppo corta');
             return false;
         }
     });
