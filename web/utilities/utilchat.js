851950da363ebbca6aa3ed402ad90816681a4455
facste <20009959@studenti.uniupo.it>
2017-05-30 14:20:40 +0200

    jquery riconosce click di un elemento

--- web/utilities/utilchat.js
+++ web/utilities/utilchat.js
@@ -3,3 +3,11 @@ $(document).ready(function()
         $("#lm").tablesorter( {sortList: [[0,0]]} );
     }
 );
+$(document).ready(function(){
+$("img").click(function() {
+    var $tr = $(this).parents("tr");
+    alert( $tr.find("td").eq(0).html() ); // mittente
+    alert( $tr.find("td").eq(1).html() ); // destinatario
+    alert( $tr.find("td").eq(2).html() ); // messaggio
+});
+});
